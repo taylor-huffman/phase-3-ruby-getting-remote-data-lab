df3d12b47ad1cf7059cad9d2dbfd4c0b5a23e78c
Taylor Huffman <taylor@crucialartists.com>
2022-06-09 09:14:46 -0500

    commit tree e1a7d4d3b188a3c49f271f1fbbd338e5a1504d2c

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    # attr_accessor :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        response_body = JSON.parse(self.get_response_body)
+        response_body
+    end
+
+end
